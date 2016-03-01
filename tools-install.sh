1c4ce95ded7188ccae4246e0910a442020fc4388
Rawaa Qasha <r.qasha@newcastle.ac.uk>
2016-03-01 15:57:39 +0000

    Update tools-install.sh

--- tools-install.sh
+++ tools-install.sh
@@ -7,27 +7,30 @@ if [[ -z ${Wget} ]]; then
    echo "wget installation"
    sudo apt-get install -y wget
 fi
-
+if [[ ! -d ~/temp-install ]]; then
+   mkdir ~/temp-install
+fi
 # docker installation
 Dock=$(which docker)
 echo "${Dock}"
 if [[ -z ${Dock} ]]; then
    echo "Docker installation"
-   if [ ! -f temp-install/docker-installation.sh ]; then
-      wget -O temp-install/docker-installation.sh https://github.com/WorkflowCenter-Repositories/ToolsInstallationScripts/raw/master/docker-installation.sh
-      chmod u+x temp-install/docker-installation.sh
+   if [ ! -f ~/temp-install/docker-installation.sh ]; then
+      wget -O ~/temp-install/docker-installation.sh https://github.com/WorkflowCenter-Repositories/ToolsInstallationScripts/raw/master/docker-installation.sh
+      chmod u+x ~/temp-install/docker-installation.sh
    fi
-   ./temp-install/docker-installation.sh
+   . ~/temp-install/docker-installation.sh
 fi
 
 #Cloudify configuration and installation
 
 if [[ ! -d ~/WF-Cloudify ]]; then
    echo "Cloudify installation"
-   mkdir temp-install
-   wget -O temp-install/cloudify-install.sh https://github.com/WorkflowCenter-Repositories/ToolsInstallationScripts/raw/master/cloudify-install.sh
-   chmod u+x ~temp-install/cloudify-install.sh
-   . ./temp-install/cloudify-install.sh
+  if [ ! -f ~/temp-install/cloudify-install.sh ]; then
+   wget -O ~/temp-install/cloudify-install.sh https://github.com/WorkflowCenter-Repositories/ToolsInstallationScripts/raw/master/cloudify-install.sh
+  fi
+   chmod u+x ~/temp-install/cloudify-install.sh
+   . . ~/temp-install/cloudify-install.sh
 else 
    source ~/WF-Cloudify/bin/activate
 fi
